f8f1a0a56aba8b40be1958f0b69ba242cf1b61eb
SARU <ndimbiarivelo.tsantatsiory@gmail.com>
2025-01-22 11:01:31 +0400

    delivery

--- siteteste/script.js
+++ siteteste/script.js
@@ -1,6 +1,6 @@
-const menu = document.querySelector(".menu")
-        const navLinks = document.querySelector(".nav-links")
- 
-        menu.addEventListener('click',()=>{
-        navLinks.classList.toggle('mobile-menu')
-        })
+function order() {
+    window.location.href = "/siteteste/book/book.html";
+}
+function delivery() {
+    window.location.href = "/siteteste/location/delivery.html"
+}
